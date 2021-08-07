9c70a6761570c8e909305f2458cdb5c15b82532c
121amylin <121amylin@gmail.com>
2021-08-07 23:18:01 +0800

    vue create & add json

--- readme.md
+++ readme.md
@@ -1,2 +1,1 @@
-
-test
+[YDOOK：Vue3.0: vue-cli4.5：訪問本地json文件並讀取數據使用axios](https://blog.csdn.net/weixin_42255190/article/details/113702675)
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vue from 'vue'
+Vue.use(VueAxios, axios)
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  mounted () {
+    axios.get('./db.json').then(response => {
+      console.log(response.data)
+    })
+  }
+}
+</script>
