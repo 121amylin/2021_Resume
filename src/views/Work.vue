feadb0a0cffcbe4a2f753db92702bdb293145f1c
121amylin <121amylin@gmail.com>
2021-08-20 01:54:53 +0800

    work page

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="home" v-if="isloadend">
-    <PictureList></PictureList>
-    <TableArr></TableArr>
-    <TableObj></TableObj>
+    <div class="section container">
+      <div class="downmenu" :class="{ active: isShow_menu }">
+        <div @click="togglemenuHandle">
+          <p>請選擇作品類型</p>
+          <ul>
+            <li
+              v-for="(item, index) in get_data"
+              :key="index"
+              @click="changeClassHandle(item)"
+              v-show="item.type != ''"
+            >
+              <p>{{ item.name }}</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <compoment :is="type" :pdate="show_data"> </compoment>
+    </div>
   </div>
 </template>
 
@@ -16,7 +31,25 @@ export default {
   components: { PictureList, TableArr, TableObj },
   data () {
     return {
-      isloadend: false
+      isloadend: false,
+      isShow_menu: false,
+      type: '',
+      show_data: {}
+    }
+  },
+  computed: {
+    get_data () {
+      return this.$store.state.work
+    }
+  },
+  methods: {
+    changeClassHandle (item) {
+      console.log(item)
+      this.type = item.type
+      this.show_data = item.class_data
+    },
+    togglemenuHandle () {
+      this.isShow_menu = !this.isShow_menu
     }
   },
   mounted () {
@@ -28,6 +61,32 @@ export default {
 </script>
 
 <style lang="scss">
-// @import '../scss/common';
+@import '../scss/common';
+.downmenu {
+  position: relative;
+  p {
+    margin: 0;
+  }
+  ul {
+    display: none;
+    position: absolute;
+    padding-left: 0;
+    background-color: #fff;
+    margin: 0;
+    list-style: none;
+    min-width: 200px;
+    li {
+      display: block;
+      cursor: pointer;
+      padding: 10px;
+    }
+  }
+  &.active {
+    ul {
+      display: block;
+    }
+  }
+}
+
 //
 </style>
